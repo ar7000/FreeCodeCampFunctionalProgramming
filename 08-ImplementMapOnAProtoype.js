81b618a47ac41c0f79d2f749d0f9e05fb554cb9f
Ash Robinson <a_p_robinson@icloud.com>
2019-11-21 23:51:42 +0000

    Completed tasks 6 through 9 on higher order functions

---
+++ 08-ImplementMapOnAProtoype.js
@@ -0,0 +1,16 @@
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function (callback) {
+    var newArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i]))
+    }
+
+    return newArray;
+
+};
+
+var new_s = s.myMap(function (item) {
+    return item * 2;
+});
